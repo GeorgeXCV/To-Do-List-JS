521f9f13b237f4e7645fbb5d147d1e08a15fd2cc
George Ashton <georgeashton95@gmail.com>
2019-03-07 18:45:57 +0000

    Update

--- todo.js
+++ todo.js
@@ -6,7 +6,7 @@ todos[0] = 'item 1 updated'
 todos.splice(0, 1)
 
 function displayTodos() {
-    console.log('My todos:', todos);
+    console.log('My Todos:', todos);
 }
 
 function addTodo(todo) {
@@ -29,7 +29,7 @@ function deleteTodo(position) {
 var todoList = {
     todos: ['item 1', 'item 2', 'item 3'],
     displayTodos: function() {
-        console.log('My Todos', this.todos);
+        console.log('My Todos:', this.todos);
     },
     addTodo: function(todo) {
         this.todos.push(todo);
@@ -50,7 +50,7 @@ var todoList = {
 var todoList = {
     todos: [],
     displayTodos: function() {
-        console.log('My Todos', this.todos);
+        console.log('My Todos:', this.todos);
     },
     addTodo: function(todoText) {
         this.todos.push({
@@ -74,3 +74,42 @@ var todoList = {
     }
 }; 
 
+// V4
+
+var todoList = {
+    todos: [],
+    displayTodos: function() {
+        if (this.todos.length === 0) {
+            console.log('Your todo list is empty!');
+        } else {
+            console.log('My Todos:');
+            for (var i = 0; i < this.todos.length; i++) {
+                if (this.todos[i].completed === true) {
+                console.log('(x)', this.todos[i].todoText);
+              } else {
+                console.log('( )', this.todos[i].todoText);
+            }
+          }
+        }   
+    },
+    addTodo: function(todoText) {
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
+        this.displayTodos();
+    },
+    changeTodo: function(position, todoText) {
+        this.todos[position].todoText = todoText;
+        this.displayTodos(); 
+    },
+    deleteTodo: function(position) {
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    },
+    toggleCompleted: function(position) {
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+    }
+}; 
