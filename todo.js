b99ed3e960e5e3b4f1202f17a392ed263ffc86c1
George Ashton <georgeashton95@gmail.com>
2019-03-10 11:04:45 +0000

    Final

--- todo.js
+++ todo.js
@@ -20,21 +20,20 @@ var todoList = {
         var totalTodos = this.todos.length;
         var completedTodos = 0;
 
-        for (var i = 0; i < totalTodos; i++) {
-            if (this.todos[i].completed === true) {
+        this.todos.forEach(function(todo) {
+            if (todo.completed === true) {
                 completedTodos++;
             }
-        }
+        });
 
-        if (completedTodos === totalTodos) {
-            for (var i = 0; i < totalTodos; i++) {
-                this.todos[i].completed = false;
-            } 
-        }  else {
-                for (var i = 0; < totalTodos; i++) {
-                    this.todos[i].completed = true; 
-                }
+        this.todos.forEach(function(todo) {
+            if (completedTodos === totalTodos) {
+                todo.completed = false;
+            } else {
+                todo.completed = true; 
             }
+        });
+
     }
 }; 
 
@@ -78,22 +77,24 @@ var view = {
     displayTodos: function() {
         var todosUl = document.querySelector('ul');
         todoaUl.innerHTML = '';
-        for (var i = 0; i < todoList.todos.length; i++) {
+
+        todoList.todos.forEach(function(todo, position) {
             var todoLi = document.createElement('li');
-            var todo = todoList.todos[i];
             var todoTextWithCompletion = '';
 
             if (todo.completed === true) {
-                todoTextWithCompletion = '(x) ' + todo.todoText;
+              todoTextWithCompletion = '(x) ' + todo.todoText;
             } else {
-                todoTextWithCompletion = '( ) ' + todo.todoText;
-            }
-
-            todoLi.id = i;
+            todoTextWithCompletion = '( ) ' + todo.todoText;
+             }
+             
+            todoLi.id = position;
             todoLi.textContent = todoTextWithCompletion; 
             todoLi.appendChild(this.createDeleteButton());
             todosUl.appendChild(todoLi);
-        }
+        
+        }, this);
+
     },
     createDeleteButton: function() {
         var deleteButton = document.createElement('button');
